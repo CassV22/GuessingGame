073445a1c41fe090700121a994b155429a9d5742
CassV22 <120053041+CassV22@users.noreply.github.com>
2024-01-30 01:53:05 -0500

    file containing game data; clues, images, answers

---
+++ data.js
@@ -0,0 +1,77 @@
+const wordList = [
+    {
+        word: "3d printer",
+        image: "./images/3d_printer.jpg",
+        clue: "Machine used to create 3D solid objects from a digital model by laying down layers of molten material, like plastic"
+    },
+    {
+       word: "volt meter",
+       image: "./images/voltmeter.jpg",
+       clue: "Insturment used for measuring electric potential difference between two points in an electric circuit"
+    },
+    {
+        word: "sewing machine",
+        image: "./images/sewing-machine.jpg",
+        clue: "Machine used to join fabrics or other materials together with thread"
+     },
+     {
+        word: "laser cutter",
+        image: "./images/laser_cutter.jpeg",
+        clue: "Prototyping and manufacturing tool used to cut and etch into flat material"
+     },
+     {
+        word: "jigsaw",
+        image: "./images/jigsaw.jpg",
+        clue: "Power tool used for cutting curved lines, circles, and unique shapes in to dfferent materials"
+     },
+     {
+        word: "miter saw",
+        image: "./images/mitersaw.jpg",
+        clue: "Tool used for making cuts at different angles"
+     },
+     {
+        word: "screwdriver",
+        image: "./images/screwdriver.jpg",
+        clue: "Hand tool used for the insertion and removal of screws"
+     },
+     {
+        word: "T square",
+        image: "./images/tsquare.jpg",
+        clue: "Technical tool used in drawing as a guide for drawing straight horizontal lines on a drafting table"
+     },
+     {
+        word: "pliers",
+        image: "./images/pliers.png",
+        clue: "Hand tool used to grip objects firmly"
+     },
+     {
+        word: "putty knife",
+        image: "./images/putty_knife.jpeg",
+        clue: "Hand tool used to apply and spread materials"
+     },
+     {
+        word: "safety goggles",
+        image: "./images/goggles.jpg",
+        clue: "Protective gear that provides eye protection against dust, mists, splashes, and impact"
+     },
+     {
+        word: "work gloves",
+        image: "./images/work_gloves.jpg",
+        clue: "PPE worn during projects that cover and protect the hands from potential cuts, blisters, splinters, heat, or chemical burns"
+     },
+     {
+        word: "ear plugs",
+        image: "./images/earplugs.jpg",
+        clue: "PPE worn to protect ears from loud noises or the entry of liquids, dust, excessive wind, and othe foreign bodies"
+     },
+     {
+        word: "epoxy",
+        image: "./images/epoxy.jpg",
+        clue: "An extremely durable, waterproof, multi-purpose glue"
+     },
+     {
+        word: "wire stripper",
+        image: "./images/wirestripper.jpg",
+        clue: "A handheld tool used to remove the insulation from electric wires"
+     }    
+];
